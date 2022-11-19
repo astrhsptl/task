28a9242ae8ab9278d228b2222e61a3d69e8a4a80
astrhsptl <dctrback@bk.ru>
2022-11-19 23:54:51 +0300

    final commit

--- coder.py
+++ coder.py
@@ -4,16 +4,11 @@ from compressor import archive
 from preprocessor import getPreprocessedImage
 
 
-if __name__ == '__main__':
-    print('''Hello! 
-I can coding and save ur image)''')
-    imageName = input('Just enter the image name here please =): ')
-    print('''Okay, thanks)
-Next step: ''')
+def main():
+    imageName = input('Just enter the image name here please =) (!full path): ')
     archiveName = input('Enter archive name, please =): ')
-    print('''Need u to get intermediate result? 
-(y - yes, other - no)''')
     intermadiate = True if input('Enter, please: ').capitalize().strip() == 'Y'  else False
     print('''Just wait)))''')
     Image.fromarray(getPreprocessedImage(imageName, intermadiate)).save(imageName)
     archive(archiveName, imageName)
+    print('Have a good day =)')
--- decoder.py
+++ decoder.py
@@ -1,18 +1,7 @@
-'''
-1) get archive name
-2) extract image
-'''
-
-from PIL import Image
-
 from compressor import dearchive
 
 
-if __name__ == '__main__':
-    print('''Hello! 
-I can decoding and save ur image)''')
+def main():
     archiveName = input('Just enter the archive name here please =): ')
-    print('''And last step)''')
-    excDirectory = input('Enter the exctractall directory, please: ')
+    excDirectory = input('Enter the exctractall directory, please (full path): ')
     dearchive(archiveName, excDirectory)
-    print('Have a fun day)')
